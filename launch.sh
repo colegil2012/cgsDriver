54dbd47a64e70ee0dcaf6a246a1ce354b4b6dbca
Christopher Gilbert <colegil2012@gmail.com>
2026-04-19 17:39:27 -0400

    fix for launch script to allow js and other files

--- launch.sh
+++ launch.sh
@@ -7,4 +7,5 @@ exec chromium \
   --no-first-run \
   --ozone-platform=wayland \
   --password-store=basic \
+  --allow-file-access-from-files \
   --app=file:///home/druid-mobile/celtech/app/index.html
